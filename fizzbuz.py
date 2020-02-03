53418cc9325ca583395164ff10e822c0579c9737
vanshika28 <vanshikanonu1999@gmail.com>
2020-02-04 00:53:58 +0530

    change in fizzbuz

--- fizzbuz.py
+++ fizzbuz.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 21 11:49:36 2020
 
-@author: Vanshika Garg
-"""
 a = int(input("Enter the number"))
 for i in range (1,a+1):
     if(i%3==0) & (i%5==0):
