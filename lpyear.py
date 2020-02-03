f94ffcc63afb0e43864d8d8393bc907659131db9
vanshika28 <vanshikanonu1999@gmail.com>
2020-02-03 23:10:08 +0530

    Change in a leap year function

--- lpyear.py
+++ lpyear.py
@@ -1,21 +1,11 @@
 def is_leap(year):
-        leap = False
-
-        if year % 4!= 0:
-            leap = False
-                
-        elif year % 4== 0:
-            if year % 400== 0:
-                leap = True
-                  
-            elif year % 100==0 & year % 4 ==0:
-                 leap = False
-                  
-            else:
-                leap = True
-        return leap
+    if year%4==0and(year%400==0 or year%100!=0) :
+        return True
         
-
-         
-year=int(input())
-
+    else:
+        return False
+    
+    # Write your logic here
+    #return leap
+year = int(input())
+print(is_leap(year))
