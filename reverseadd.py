4f74c400fdba52d659fcb52664e0754c52f4c5c3
vanshika28 <vanshikanonu1999@gmail.com>
2020-02-04 13:39:58 +0530

    reverseadd.py

---
+++ reverseadd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 23:53:45 2020
+
+@author: Vanshika Garg
+"""
+a = input("enter the numbers in the list")
+l= [ int(i) for i in a.split(" ") ]
+b= l[ : : -1]
+print (l)
+print(b)
+c=[]
+for i in range(0,len(l)):
+    c.append(l[i]+b[i])
+print(c)
