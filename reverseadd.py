cb60e4677b10d0b7fd01e9f4b20761229a3d024f
vanshika28 <vanshikanonu1999@gmail.com>
2020-02-04 13:41:25 +0530

    reverseadd.py

--- reverseadd.py
+++ reverseadd.py
@@ -4,7 +4,7 @@ Created on Mon Jan 20 23:53:45 2020
 
 @author: Vanshika Garg
 """
-a = input("enter the numbers in the list")
+a = input("enter  the number in the list")
 l= [ int(i) for i in a.split(" ") ]
 b= l[ : : -1]
 print (l)
